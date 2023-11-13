0d99d8cdb21a1ec82a8676210727826e04975ff4
markeltskhadadze <markeltskhadadze@gmail.com>
2023-11-13 10:36:58 +0400

    deploy heroku

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // @ts-ignore
-  experimental: {
-    inlineSSRStyles: false
-  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
